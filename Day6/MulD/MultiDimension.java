b4a85d3337ba370248c82e31a35de190d8ada2d0
Poorna Shri <poornashri2703@gmail.com>
2024-01-13 23:11:41 +0530

    Array,2DArray,input and output,columnnotfixed,passing Function Value

---
+++ Day6/MulD/MultiDimension.java
@@ -0,0 +1,72 @@
+//2D arrays
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MultiDimension {
+    public static void main(String[] args) {
+        /*
+             1 2 3
+             4 5 6
+             7 8 9
+        */
+        Scanner in = new Scanner(System.in);
+//        int[][] arr = new int[3][];//we need to mension the ra=ow size of the array and the column size is not need
+
+//        int[][] arr = {
+//                {1, 2, 3}, // 0th index
+//                {4, 5}, // 1st index
+//                {6, 7, 8, 9} // 2nd index -> arr[2] = {6, 7, 8, 9}
+//        };
+
+        int[][] arr = new int[3][3];
+        System.out.println(arr.length); // no of rows
+        // input
+
+        for (int row = 0; row < arr.length; row++) {
+            // for each col in every row
+            for (int col = 0; col < arr[row].length; col++) {
+                arr[row][col] = in.nextInt();
+            }
+        }
+
+        // output
+//        for (int row = 0; row < arr.length; row++) {
+//            // for each col in every row
+//            for (int col = 0; col < arr[row].length; col++) {
+//                System.out.print(arr[row][col] + " ");
+//            }
+//            System.out.println();
+//        }
+
+        // output
+//        for (int row = 0; row < arr.length; row++) {
+//            System.out.println(Arrays.toString(arr[row]));
+//        }
+
+        for(int[] a : arr) {
+            System.out.println(Arrays.toString(a));
+        }
+    }
+}
+
+// column is not defined 
+//we can use this method arr[row].length
+
+
+// public class ColNoFixed {
+//     public static void main(String[] args) {
+//         int[][] arr = {
+//                 {1, 2, 3, 4},
+//                 {5, 6},
+//                 {7, 8, 9}
+//         };
+
+//         for (int row = 0; row < arr.length; row++) {
+//             for (int col = 0; col < arr[row].length; col++) {
+//                 System.out.print(arr[row][col] + " ");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
