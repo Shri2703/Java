91ccdc37ea708092b35b8d9f60ee39b88655d5e2
Shri2703 <poornashri2703@gmail.com>
2024-10-21 22:13:22 +0530

    patterns

--- TakeUForword/Step1/Build-upLogicalThinking/LogicalThinking.java
+++ TakeUForword/Step1/Build-upLogicalThinking/LogicalThinking.java
@@ -28,6 +28,35 @@ public class LogicalThinking{
     // * * *
     // * * * *
     // * * * * *
+    // static void Pattern1(int num){
+    //     for(int i= 1; i <= num;i++){
+    //         for(int j=1; j <= i ;j++){
+    //             System.out.print(j+" ");
+    //         }
+    //         System.out.println();
+    //     }
+    // }
+    
+    // 1
+    // 1 2
+    // 1 2 3
+    // 1 2 3 4
+    // 1 2 3 4 5
+    
+    static void Pattern1(int num){
+        for(int i= 1; i <= num;i++){
+            for(int j=1; j <= i ;j++){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+    1
+    2 2
+    3 3 3
+    4 4 4 4
+    5 5 5 5 5
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the number");
