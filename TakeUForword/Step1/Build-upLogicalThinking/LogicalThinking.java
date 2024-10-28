f408a7714f7d064826013a554fe69ee038d0ce36
Shri2703 <poornashri2703@gmail.com>
2024-10-28 21:57:08 +0530

    task

--- TakeUForword/Step1/Build-upLogicalThinking/LogicalThinking.java
+++ TakeUForword/Step1/Build-upLogicalThinking/LogicalThinking.java
@@ -98,7 +98,7 @@ public class LogicalThinking{
     //  ***
     // *****
 
-    // static void Pattern1(int num){
+    // static void Pattern2(int num){
     //     for(int i = 0; i < num; i++){
     //         for(int j = 0;j < i;j++){
     //             System.out.print(" ");
@@ -116,13 +116,101 @@ public class LogicalThinking{
     // *****
     //  ***
     //   *
+
+//     static void Pattern1(int num){
+//         for(int i = 0; i< num; i++){
+//             for(int j = 0;j < num-i-1;j++){
+//                 System.out.print(" ");
+//             }
+//             for(int j = 0;j < 2*i+1;j++){
+//                 System.out.print("*");
+//             }
+//             for(int j = 0;j < num-i-1;j++){
+//                 System.out.print(" ");
+//             }
+            
+//             System.out.println();
+//         }
+//     }
+//     static void Pattern2(int num){
+//         for(int i = 0; i < num; i++){
+//             for(int j = 0;j < i;j++){
+//                 System.out.print(" ");
+//             }
+//             for(int j = 0;j < 2*num -(2*i +1);j++){
+//                 System.out.print("*");
+//             }
+//             for(int j = 0;j < i;j++){
+//                 System.out.print(" ");
+//             }
+            
+//             System.out.println();
+//         }
+//     }
+//     *
+//    ***
+//   *****
+//  *******
+// *********
+// *********
+//  *******
+//   *****
+//    ***
+//     *
+    // static void Pattern1(int num){
+    //     for(int i = 0; i< num; i++){
+    //         for(int j = 0;j < 2*i+1;j++){
+    //             System.out.print("*");
+    //         }
+    //         System.out.println();
+    //     }
+    // }
+    // static void Pattern2(int num){
+    //     for(int i = 0; i < num; i++){
+    //         for(int j = 0;j < 2*num -(2*i +1);j++){
+    //             System.out.print("*");
+    //         }
+    //         System.out.println();
+    //     }
+    // }
+    // *
+    // ***
+    // *****
+    // *******
+    // *********
+    // *********
+    // *******
+    // *****
+    // ***
+    // *
+    // static void Pattern1(int num){
+    //     for (int i =1;i <= 2*num - 1;i++){
+    //         int stars = i;
+
+    //         if (i > num) stars = 2*num-i;
+    //         for(int j =1;j<= stars;j++){
+    //             System.out.print("*");
+    //         }
+    //         System.out.println();
+    //     }
+    // }
+
+    // *
+    // **
+    // ***
+    // ****
+    // *****
+    // ****
+    // ***
+    // **
+    // *
+    
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the number");
         int num = input.nextInt();
-        
         Pattern1(num);
-
-
+        // Pattern2(num);
     }
 }
